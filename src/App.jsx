91ed11c9726875670de28da0520dcdd485781a40
Yan Luka Keita <yanlukakeita2000@gmail.com>
2024-01-30 15:14:21 +0100

    user detail page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AddEvent from "./pages/AddEvent";
 import FallbackLoadingScreen from "./components/loading/FallbackLoadingScreen";
 const Protector = lazy(() => import("./Protect/Protector"));
 import { EventDetails } from "./pages/EventDetails";
+import { UserProfile } from "./pages/UserProfile";
 
 function App() {
   return (
@@ -14,11 +15,13 @@ function App() {
       <BrowserRouter>
         <Suspense fallback={<FallbackLoadingScreen />}>
           <Routes>
-            <Route element={<LoginPage />} />
-            <Route element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+
             <Route element={<Protector />}>
               <Route path="/eventdetails/:id" element={<EventDetails />} />
               <Route path="/event/add" element={<AddEvent />} />
+              <Route path="/user" element={<UserProfile />} />
             </Route>
           </Routes>
         </Suspense>
--- src/Protect/Protector.jsx
+++ src/Protect/Protector.jsx
@@ -1,13 +1,13 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import PocketBase from 'pocketbase';
-import useLocalStorage from '../hooks/useLocalStorage';
-import { getUserExample } from '../utils/fetchData';
-import { useEffect } from 'react';
+import { Navigate, Outlet } from "react-router-dom";
+import PocketBase from "pocketbase";
+import useLocalStorage from "../hooks/useLocalStorage";
+import { getUserExample } from "../utils/fetchData";
+import { useEffect } from "react";
 
 const pb = new PocketBase(`${import.meta.env.VITE_POCKET_FETCH_URL}`);
 
 const Protector = () => {
-  const [isLogin] = useLocalStorage('pocketbase_auth', null);
+  const [isLogin] = useLocalStorage("pocketbase_auth", null);
   // const isLogin = useLocalStorage('pocketbase_auth', null);
 
   useEffect(() => {
@@ -16,18 +16,18 @@ const Protector = () => {
       const userData = await getUserExample(pb);
       if (!userData) {
         // Wenn wir unsere Userdaten nicht abrufen können leiten wir uns zur Login Seite um
-        return <Navigate to={'/login'} />;
+        return <Navigate to={"/login"} />;
       }
     };
 
     if (!isLogin) {
-      fetchData();
+      //fetchData();
     }
   }, [isLogin]);
 
   if (!isLogin) {
     // Wenn wir unsere Userdaten nicht abrufen können leiten wir uns zur Login Seite um
-    return <Navigate to={'/login'} />;
+    return <Navigate to={"/login"} />;
   }
 
   return (
