7653d13ce4813b6271939cb92ce1e74d09eca64a
Yan Luka Keita <yanlukakeita2000@gmail.com>
2024-01-30 09:51:20 +0100

    eingerückt

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import { Suspense, lazy } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Protector from './Protect/Protector';
-import AddEvent from './pages/AddEvent';
-import FallbackLoadingScreen from './components/loading/FallbackLoadingScreen';
+import AddEvent from "./pages/AddEvent";
+import FallbackLoadingScreen from "./components/loading/FallbackLoadingScreen";
 
-const Protector = lazy(() => import('./Protect/Protector'));
+const Protector = lazy(() => import("./Protect/Protector"));
 
 import { EventDetails } from "./assets/pages/EventDetails";
-
 
 function App() {
   return (
     <>
-
       <BrowserRouter>
         <Suspense fallback={<FallbackLoadingScreen />}>
           <Routes>
-    <Route element={<LoginPage />}/>
-      <Route element={<RegisterPage />}/>
+            <Route element={<LoginPage />} />
+            <Route element={<RegisterPage />} />
             <Route element={<Protector />}>
-              <Route path="/eventdetails/:id" element={<EventDetails />}/>
+              <Route path="/eventdetails/:id" element={<EventDetails />} />
               <Route path="/event/add" element={<AddEvent />} />
             </Route>
           </Routes>
         </Suspense>
       </BrowserRouter>
-
-
     </>
   );
 }
