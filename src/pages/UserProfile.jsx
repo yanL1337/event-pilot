ca4eab94081706b9cb8b782597acbb053f0f7aa0
Yan Luka Keita <yanlukakeita2000@gmail.com>
2024-01-31 12:11:01 +0100

    edit styling

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,48 +1,94 @@
 import React, { useEffect, useState } from "react";
 import pb from "../lib/pocketbase.js";
+import editProfile from "../../public/images/Icon.svg";
 
 export const UserProfile = () => {
   const [user, setUser] = useState();
+  const [following, setFollowing] = useState();
 
   useEffect(() => {
     const getUser = async () => {
       const record = await pb.collection("users").getOne(pb.authStore.model.id);
       setUser(record);
     };
+
     getUser();
+    //console.log(user);
   }, []);
 
+  useEffect(() => {
+    let count = 0;
+    const getFollowing = async () => {
+      const records = await pb.collection("users").getFullList();
+
+      records.forEach((userRecord) =>
+        userRecord?.follower.forEach((follower) =>
+          follower == user?.id ? count++ : null
+        )
+      );
+      setFollowing(count);
+    };
+    getFollowing();
+  }, [user]);
+
+  console.log(user);
+
   return (
-    <div className="my-profile">
-      <div className="div-2">
-        <div className="text">
-          <div className="d">Edit Profile</div>
-          <img className="icon" alt="Icon" src="icon.svg" />
+    <div style={{}}>
+      <div
+        style={{
+          padding: "5vh 10vw",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div className="top-bar">
+          <div className="name"> {`${user?.firstname} ${user?.lastname}`}</div>
         </div>
+
+        <img
+          className="image"
+          alt="Image"
+          src={`https://event-pilot.pockethost.io/api/files/${user?.collectionId}/${user?.id}/${user?.profilImage}`}
+        />
+
+        <div
+          style={{
+            borderRadius: "10px",
+            border: "1.5px solid var(--4, #777BF3)",
+            display: "flex",
+            alignItems: "center",
+            width: "30vw",
+            padding: "1vh 1vw",
+          }}
+        >
+          <img className="icon" alt="Icon" src={editProfile} />
+          <p className="d">Edit Profile</p>
+        </div>
+
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gap: "5vw",
+          }}
+        >
+          <div style={{ textAlign: "center" }}>
+            <div className="text-wrapper-3">Following</div>
+            <div className="text-wrapper-4">{following}</div>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <div className="text-wrapper-3">Followers</div>
+            <div className="text-wrapper-4">{user?.follower.length}</div>
+          </div>
+        </div>
+
         <div className="about-me">
           <p className="p">
             <span className="span">{user?.description}</span>
           </p>
           <div className="headline">About Me</div>
-        </div>
-        <div className="stats">
-          <div className="overlap-3">
-            <div className="text-wrapper-3">Following</div>
-            <div className="text-wrapper-4">{"Following"}</div>
-          </div>
-          <div className="overlap-4">
-            <div className="text-wrapper-3">Followers</div>
-            <div className="text-wrapper-4">{user?.follower.length}</div>
-          </div>
-          <img className="split" alt="Split" src="split.svg" />
-        </div>
-        <img
-          className="image"
-          alt="Image"
-          src={`https://event-pilot.pockethost.io/api/files/${user?.collectionId}/${user?.id}/${user?.profileImage}`}
-        />
-        <div className="top-bar">
-          <div className="name"> {`${user?.firstname} ${user?.username}`}</div>
         </div>
 
         <div className="interests">
