03ffd14fbf356a3b41631859b0b2e6b0a265212b
oschwill <o.schwill@web.de>
2024-02-06 09:31:36 +0100

    fragment

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,24 +1,21 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 /* CSS */
-import styles from "./css/Favorites.module.css";
-import style from "./css/CreatorProfile.module.css";
-import { useEffect, useState } from "react";
-import {
-  getEventFavoritesData,
-  getRegisteredEventsByUser,
-} from "../utils/fetchData";
-import LoadingElement from "../components/loading/LoadingElement";
-import OutputItem from "../components/general/OutputItem";
-import DynamicTriggerButton from "../components/buttons/DynamicTriggerButton";
-import { useNavigate } from "react-router-dom";
-import ToggleFavoritesBar from "../components/events/favorites/ToggleFavoritesBar";
-import LoadMoreButton from "../components/buttons/LoadMoreButton";
+import styles from './css/Favorites.module.css';
+import style from './css/CreatorProfile.module.css';
+import { useEffect, useState } from 'react';
+import { getEventFavoritesData, getRegisteredEventsByUser } from '../utils/fetchData';
+import LoadingElement from '../components/loading/LoadingElement';
+import OutputItem from '../components/general/OutputItem';
+import DynamicTriggerButton from '../components/buttons/DynamicTriggerButton';
+import { useNavigate } from 'react-router-dom';
+import ToggleFavoritesBar from '../components/events/favorites/ToggleFavoritesBar';
+import LoadMoreButton from '../components/buttons/LoadMoreButton';
 
 const eventsPerRow = 6;
 
 export function Favorites({ children }) {
-  const [toggleButton, setToggleButton] = useState("up");
+  const [toggleButton, setToggleButton] = useState('up');
   const [isLoading, setIsLoading] = useState(false);
   const [favEvents, setFavEvents] = useState({});
   const [registeredEvents, setRegisteredEvents] = useState([]);
@@ -27,10 +24,7 @@ export function Favorites({ children }) {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (
-      (favEvents.length === 0 || registeredEvents.length === 0) &&
-      !isLoading
-    ) {
+    if ((favEvents.length === 0 || registeredEvents.length === 0) && !isLoading) {
       fetchFavoriteData();
     }
 
@@ -43,12 +37,8 @@ export function Favorites({ children }) {
 
     if (response.length > 0) {
       // Wir erstellen nun aus response ein upcoming und ein past Event array
-      const pastEvents = response.filter(
-        (events) => new Date(events.date) < new Date()
-      );
-      const upComingEvents = response.filter(
-        (events) => new Date(events.date) >= new Date()
-      );
+      const pastEvents = response.filter((events) => new Date(events.date) < new Date());
+      const upComingEvents = response.filter((events) => new Date(events.date) >= new Date());
 
       setFavEvents(() => ({ pastEvents, upComingEvents }));
     }
@@ -75,7 +65,7 @@ export function Favorites({ children }) {
   };
 
   const navigateToSearchEvents = () => {
-    navigate("/event/search");
+    navigate('/event/search');
   };
 
   const handleMoreEvents = () => {
@@ -83,94 +73,91 @@ export function Favorites({ children }) {
   };
 
   return (
-    <section className={style.wrapper}>
-      <header>
-        <section>
-          <article className={styles.header_article}>
-            <h2>
-              {toggleButton === "up" ? "Liked Events" : "Registered Events"}
-            </h2>
-          </article>
-        </section>
-      </header>
-      <main>
-        <section className={styles.favorite_section}>
-          <article>
-            <ToggleFavoritesBar
-              styles={styles}
-              onHandleToggleButton={handleToggleButton}
-              toggleButton={toggleButton}
-            />
-          </article>
-          <article className={styles.favorite_output_wrapper}>
-            {isLoading ? (
-              <div style={{ marginTop: "200px" }}>
-                <LoadingElement />
-              </div>
-            ) : (
-              <>
-                {toggleButton === "up" &&
-                favEvents.upComingEvents &&
-                favEvents.upComingEvents.length > 0 ? (
-                  <>
-                    {favEvents.upComingEvents?.slice(0, next).map((event) => (
-                      <OutputItem
-                        data={event}
-                        key={event.id}
-                        isOnFavSite={true}
-                        registeredEvents={registeredEvents}
-                      />
-                    ))}
-                    {next < favEvents.upComingEvents?.length && (
-                      <LoadMoreButton handleMoreEvents={handleMoreEvents} />
-                    )}
-                  </>
-                ) : toggleButton === "registered" &&
-                  registeredEvents &&
-                  registeredEvents.length > 0 ? (
-                  <>
-                    {registeredEvents?.slice(0, next).map((event) => (
-                      <OutputItem
-                        data={event}
-                        key={event.id}
-                        isOnFavSite={true}
-                        registeredEvents={registeredEvents}
-                      />
-                    ))}
-                    {next < registeredEvents?.length && (
-                      <LoadMoreButton handleMoreEvents={handleMoreEvents} />
-                    )}
-                  </>
-                ) : (
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      marginTop: "30%",
-                      gap: "20px",
-                    }}
-                  >
-                    <h2>No Upcoming Events</h2>
-                    <p>There are no events yet!</p>
-                  </div>
-                )}
-              </>
-            )}
-          </article>
-          <article className={styles.favorite_button_wrapper}>
-            <DynamicTriggerButton
-              hasArrow={true}
-              onTriggerEventFn={navigateToSearchEvents}
-            >
-              SEARCH EVENTS
-            </DynamicTriggerButton>
-          </article>
-        </section>
-        {children}
-      </main>
-    </section>
+    <>
+      <section className={style.wrapper}>
+        <header>
+          <section>
+            <article className={styles.header_article}>
+              <h2>{toggleButton === 'up' ? 'Liked Events' : 'Registered Events'}</h2>
+            </article>
+          </section>
+        </header>
+        <main>
+          <section className={styles.favorite_section}>
+            <article>
+              <ToggleFavoritesBar
+                styles={styles}
+                onHandleToggleButton={handleToggleButton}
+                toggleButton={toggleButton}
+              />
+            </article>
+            <article className={styles.favorite_output_wrapper}>
+              {isLoading ? (
+                <div style={{ marginTop: '200px' }}>
+                  <LoadingElement />
+                </div>
+              ) : (
+                <>
+                  {toggleButton === 'up' &&
+                  favEvents.upComingEvents &&
+                  favEvents.upComingEvents.length > 0 ? (
+                    <>
+                      {favEvents.upComingEvents?.slice(0, next).map((event) => (
+                        <OutputItem
+                          data={event}
+                          key={event.id}
+                          isOnFavSite={true}
+                          registeredEvents={registeredEvents}
+                        />
+                      ))}
+                      {next < favEvents.upComingEvents?.length && (
+                        <LoadMoreButton handleMoreEvents={handleMoreEvents} />
+                      )}
+                    </>
+                  ) : toggleButton === 'registered' &&
+                    registeredEvents &&
+                    registeredEvents.length > 0 ? (
+                    <>
+                      {registeredEvents?.slice(0, next).map((event) => (
+                        <OutputItem
+                          data={event}
+                          key={event.id}
+                          isOnFavSite={true}
+                          registeredEvents={registeredEvents}
+                        />
+                      ))}
+                      {next < registeredEvents?.length && (
+                        <LoadMoreButton handleMoreEvents={handleMoreEvents} />
+                      )}
+                    </>
+                  ) : (
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        marginTop: '30%',
+                        gap: '20px',
+                      }}
+                    >
+                      <h2>No Upcoming Events</h2>
+                      <p>There are no events yet!</p>
+                    </div>
+                  )}
+                </>
+              )}
+            </article>
+            <article className={styles.favorite_button_wrapper}>
+              <DynamicTriggerButton hasArrow={true} onTriggerEventFn={navigateToSearchEvents}>
+                SEARCH EVENTS
+              </DynamicTriggerButton>
+            </article>
+          </section>
+        </main>
+      </section>
+      {children}
+    </>
   );
 }
 
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from "react";
-import pb from "../lib/pocketbase.js";
-import editProfile from "/images/EditIcon.svg";
-import submitEdit from "/images/Arrow.svg";
-import Following from "../components/following/Following.jsx";
-import Interests from "../components/interests/Interests.jsx";
-import { CreatorEvent } from "../components/events/CreatorEvents.jsx";
-import { OwnEvent } from "../components/events/OwnEvent.jsx";
-import { Header } from "../components/header/Header.jsx";
-import style from "./css/UserProfil.module.css";
+import React, { useEffect, useState } from 'react';
+import pb from '../lib/pocketbase.js';
+import editProfile from '/images/EditIcon.svg';
+import submitEdit from '/images/Arrow.svg';
+import Following from '../components/following/Following.jsx';
+import Interests from '../components/interests/Interests.jsx';
+import { CreatorEvent } from '../components/events/CreatorEvents.jsx';
+import { OwnEvent } from '../components/events/OwnEvent.jsx';
+import { Header } from '../components/header/Header.jsx';
+import style from './css/UserProfil.module.css';
 
 export const UserProfile = ({ children }) => {
   const [user, setUser] = useState();
   const [edit, setEdit] = useState(false);
   const [changes, setChanges] = useState({});
-  const [state, setState] = useState("about");
+  const [state, setState] = useState('about');
   const [ownEvents, setOwnEvents] = useState([]);
   const [colorAbout, setColorAbout] = useState(true);
   const [colorEvents, setColorEvents] = useState(false);
 
   useEffect(() => {
     const getUser = async () => {
-      const record = await pb.collection("users").getOne(pb.authStore.model.id);
+      const record = await pb.collection('users').getOne(pb.authStore.model.id);
       setUser(record);
       setChanges(record);
     };
@@ -37,45 +37,45 @@ export const UserProfile = ({ children }) => {
     event.preventDefault();
     const formData = new FormData();
 
-    formData.append("firstname", changes.firstname);
-    formData.append("lastname", changes.lastname);
-    formData.append("description", changes.description);
+    formData.append('firstname', changes.firstname);
+    formData.append('lastname', changes.lastname);
+    formData.append('description', changes.description);
     const fileInput = document.querySelector('input[type="file"]');
 
     if (fileInput && fileInput.files[0]) {
-      formData.append("profilImage", fileInput.files[0]);
+      formData.append('profilImage', fileInput.files[0]);
     }
 
     for (let item of changes.interests) {
       console.log(item);
-      formData.append("interests", item);
+      formData.append('interests', item);
     }
 
     //formData.append("interests", changes.interests);
     try {
-      const record = await pb.collection("users").update(user.id, formData);
+      const record = await pb.collection('users').update(user.id, formData);
       setUser(record);
       setEdit(false);
     } catch (error) {
-      console.error("Failed to update", error);
+      console.error('Failed to update', error);
     }
   };
 
   // ##############################
   function about() {
-    setState("about");
+    setState('about');
     setColorAbout(true);
     setColorEvents(false);
   }
   function events() {
-    setState("events");
+    setState('events');
     setColorAbout(false);
     setColorEvents(true);
   }
 
   useEffect(() => {
     async function getOwnEvents() {
-      const ownEvents = await pb.collection("events").getFullList({
+      const ownEvents = await pb.collection('events').getFullList({
         filter: `creator="${pb.authStore.model.id}"`,
       });
       setOwnEvents(ownEvents);
@@ -86,118 +86,104 @@ export const UserProfile = ({ children }) => {
   console.log(user);
 
   //* wird angezeigt, wenn About ausgewählt ist
-  if (state === "about" && user) {
+  if (state === 'about' && user) {
     return (
-      <section className={style.wrapper}>
-        {!edit ? (
-          <>
-            <Header headertext={`${user.firstname} ${user.lastname}`} />
-            <img
-              className={style.profilimg}
-              src={`https://event-pilot.pockethost.io/api/files/${user?.collectionId}/${user?.id}/${user?.profilImage}`}
-            />
+      <>
+        <section className={style.wrapper}>
+          {!edit ? (
+            <>
+              <Header headertext={`${user.firstname} ${user.lastname}`} />
+              <img
+                className={style.profilimg}
+                src={`https://event-pilot.pockethost.io/api/files/${user?.collectionId}/${user?.id}/${user?.profilImage}`}
+              />
 
-            <div className={style.followdiv}>
-              <div className={style.follow}>
-                <Following user={user} />
-                <p className={style.light}>Following</p>
-              </div>
-              <div>
-                <p>{user?.follower.length}</p>
-                <p className={style.light}>Followers</p>
-              </div>
-            </div>
-            <div className={style.tabs}>
-              <button
-                className={colorAbout ? style.activeTab : null}
-                onClick={about}
-              >
-                ABOUT
-              </button>
-              <button
-                className={colorEvents ? style.activeTab : null}
-                onClick={events}
-              >
-                EVENTS
-              </button>
-            </div>
-
-            <div className={style.aboutme}>About Me</div>
-            <p className={style.description}>{user?.description}</p>
-
-            <Interests user={user} edit={edit} />
-            <div className={style.editbutton} onClick={() => setEdit(true)}>
-              <img className="icon" alt="Icon" src={editProfile} />
-              <p className="d">Edit Profile</p>
-            </div>
-          </>
-        ) : (
-          //* wird beim EditProfil Button angezeigt
-          //submitChanges, changes, handleInputChange
-          <>
-            <Header headertext={`Edit Profile`} />
-            <form onSubmit={submitChanges}>
-              <div className={style.editimg}>
-                <img
-                  className={style.profilimgedit}
-                  src={`https://event-pilot.pockethost.io/api/files/${user?.collectionId}/${user?.id}/${user?.profilImage}`}
-                />
-                <div className={style.imgupload}>
-                  <label htmlFor="file-input">
-                    <img
-                      style={{ width: "7vw", cursor: "pointer" }}
-                      src={editProfile}
-                    />
-                  </label>
-                  <input
-                    style={{ display: "none" }}
-                    name="profilImage"
-                    id="file-input"
-                    type="file"
-                  />
+              <div className={style.followdiv}>
+                <div className={style.follow}>
+                  <Following user={user} />
+                  <p className={style.light}>Following</p>
+                </div>
+                <div>
+                  <p>{user?.follower.length}</p>
+                  <p className={style.light}>Followers</p>
                 </div>
               </div>
+              <div className={style.tabs}>
+                <button className={colorAbout ? style.activeTab : null} onClick={about}>
+                  ABOUT
+                </button>
+                <button className={colorEvents ? style.activeTab : null} onClick={events}>
+                  EVENTS
+                </button>
+              </div>
 
-              <input
-                className={style.input}
-                name="firstname"
-                placeholder="First Name"
-                value={changes.firstname || ""}
-                onChange={handleInputChange}
-              />
-              <input
-                className={style.input}
-                name="lastname"
-                placeholder="Last Name"
-                value={changes.lastname || ""}
-                onChange={handleInputChange}
-              />
+              <div className={style.aboutme}>About Me</div>
+              <p className={style.description}>{user?.description}</p>
 
-              <textarea
-                style={{ height: "10vh", width: "70vw" }}
-                name="description"
-                placeholder="About me"
-                value={changes.description || ""}
-                onChange={handleInputChange}
-              />
+              <Interests user={user} edit={edit} />
+              <div className={style.editbutton} onClick={() => setEdit(true)}>
+                <img className="icon" alt="Icon" src={editProfile} />
+                <p className="d">Edit Profile</p>
+              </div>
+            </>
+          ) : (
+            //* wird beim EditProfil Button angezeigt
+            //submitChanges, changes, handleInputChange
+            <>
+              <Header headertext={`Edit Profile`} />
+              <form onSubmit={submitChanges}>
+                <div className={style.editimg}>
+                  <img
+                    className={style.profilimgedit}
+                    src={`https://event-pilot.pockethost.io/api/files/${user?.collectionId}/${user?.id}/${user?.profilImage}`}
+                  />
+                  <div className={style.imgupload}>
+                    <label htmlFor="file-input">
+                      <img style={{ width: '7vw', cursor: 'pointer' }} src={editProfile} />
+                    </label>
+                    <input
+                      style={{ display: 'none' }}
+                      name="profilImage"
+                      id="file-input"
+                      type="file"
+                    />
+                  </div>
+                </div>
 
-              <Interests
-                changes={changes}
-                setChanges={setChanges}
-                edit={edit}
-              />
-              <button
-                type="submit"
-                style={{ display: "flex", alignItems: "center" }}
-              >
-                <p>Save changes</p>
-                <img src={submitEdit} alt="" />
-              </button>
-            </form>
-          </>
-        )}
+                <input
+                  className={style.input}
+                  name="firstname"
+                  placeholder="First Name"
+                  value={changes.firstname || ''}
+                  onChange={handleInputChange}
+                />
+                <input
+                  className={style.input}
+                  name="lastname"
+                  placeholder="Last Name"
+                  value={changes.lastname || ''}
+                  onChange={handleInputChange}
+                />
+
+                <textarea
+                  style={{ height: '10vh', width: '70vw' }}
+                  name="description"
+                  placeholder="About me"
+                  value={changes.description || ''}
+                  onChange={handleInputChange}
+                />
+
+                <Interests changes={changes} setChanges={setChanges} edit={edit} />
+                <button type="submit" style={{ display: 'flex', alignItems: 'center' }}>
+                  <p>Save changes</p>
+                  <img src={submitEdit} alt="" />
+                </button>
+              </form>
+            </>
+          )}
+        </section>
         {children}
-      </section>
+      </>
     );
     // * wird bei Events angezeigt
   } else if (user) {
@@ -220,16 +206,10 @@ export const UserProfile = ({ children }) => {
           </div>
         </div>
         <div className={style.tabs}>
-          <button
-            className={colorAbout ? style.activeTab : null}
-            onClick={about}
-          >
+          <button className={colorAbout ? style.activeTab : null} onClick={about}>
             ABOUT
           </button>
-          <button
-            className={colorEvents ? style.activeTab : null}
-            onClick={events}
-          >
+          <button className={colorEvents ? style.activeTab : null} onClick={events}>
             EVENTS
           </button>
         </div>
