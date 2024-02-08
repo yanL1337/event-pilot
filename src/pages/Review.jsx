a0ad275f33aec97dab35042453a4d51d90d3bd8d
Yan Luka Keita <yanlukakeita2000@gmail.com>
2024-02-08 10:55:09 +0100

    textarea

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,13 +1,12 @@
-import { useParams, useNavigate } from 'react-router-dom';
-import { useState, useEffect, useRef, useContext } from 'react';
-import pb from '../lib/pocketbase';
-import style from './css/Review.module.css';
-import { Header } from '../components/header/Header';
-import { Rating } from 'react-simple-star-rating';
-import { displayFavMessage } from '../utils/helperFunction';
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef, useContext } from "react";
+import pb from "../lib/pocketbase";
+import style from "./css/Review.module.css";
+import { Header } from "../components/header/Header";
+import { Rating } from "react-simple-star-rating";
+import { displayFavMessage } from "../utils/helperFunction";
 import { SetFavoriteMessageContext, ThemeContext } from "../context/context";
-import LoadingElement from '../components/loading/LoadingElement';
-
+import LoadingElement from "../components/loading/LoadingElement";
 
 export function Review() {
   const { theme } = useContext(ThemeContext);
@@ -27,7 +26,7 @@ export function Review() {
   //   * Welcher Creator soll bewertet werden? - id anhand useParams
   useEffect(() => {
     async function getCreator() {
-      const record = await pb.collection('users').getOne(id);
+      const record = await pb.collection("users").getOne(id);
       setCreator(record);
     }
     getCreator();
@@ -51,19 +50,27 @@ export function Review() {
     };
     try {
       setIsLoading(true);
-      const response = await pb.collection('reviews').create(review);
+      const response = await pb.collection("reviews").create(review);
 
       if (response.rating > 0 && response.rating < 6) {
-        displayFavMessage(`Your review has been saved`, setFavMessage, favMessageTimer);
+        displayFavMessage(
+          `Your review has been saved`,
+          setFavMessage,
+          favMessageTimer
+        );
       } else {
-        displayFavMessage(`Oops, your review couldn't be saved.`, setFavMessage, favMessageTimer);
+        displayFavMessage(
+          `Oops, your review couldn't be saved.`,
+          setFavMessage,
+          favMessageTimer
+        );
       }
     } catch (error) {
       console.log(error);
     }
     setTimeout(() => {
       setIsLoading(false);
-      navigate(`/creator/${id}`, { state: 'review' });
+      navigate(`/creator/${id}`, { state: "review" });
     }, 1500);
   };
 
@@ -74,41 +81,47 @@ export function Review() {
 
   if (creator) {
     return (
-        <section className={theme ? style.dark : null}>
-          <article className={style.article}>
-            <Header
-              headertext={`Review ${creator.firstname}`}
-              className={style.header}
-            />
-            <main className={style.wrapper1}>
-              <div>
-                <img
-                  className={style.creatorprofil_img}
-                  src={`${pb.baseUrl}/api/files/${creator.collectionId}/${creator.id}/${creator.profilImage}`}
-                  alt="Profilbild"
+      <section className={theme ? style.dark : null}>
+        <article className={style.article}>
+          <Header
+            headertext={`Review ${creator.firstname}`}
+            className={style.header}
+          />
+          <main className={style.wrapper1}>
+            <div>
+              <img
+                className={style.creatorprofil_img}
+                src={`${pb.baseUrl}/api/files/${creator.collectionId}/${creator.id}/${creator.profilImage}`}
+                alt="Profilbild"
+              />
+            </div>
+            <div>
+              <Rating
+                onClick={handleRating}
+                fillColor="#00ECAA"
+                className={style.stars}
+              />
+              <div className={style.commentdiv}>
+                {/* <label className={style.label}>★ Your Review</label> */}
+                //#######
+                <textarea
+                  type="text"
+                  placeholder="★ Your Review"
+                  ref={commentRef}
+                  className={style.input}
                 />
               </div>
-              <div>
-                <Rating
-                  onClick={handleRating}
-                  fillColor="#00ECAA"
-                  className={style.stars}
-                />
-                <div className={style.commentdiv}>
-                  <label className={style.label}>★ Your Review</label>
-                  <input type="text" ref={commentRef} className={style.input} />
-                </div>
-              </div>
-              {!isLoading ? (
-            <button className={style.button} onClick={sendReview}>
-              SUBMIT
-            </button>
-          ) : (
-            <LoadingElement />
-          )}
-            </main>
-          </article>
-        </section>
+            </div>
+            {!isLoading ? (
+              <button className={style.button} onClick={sendReview}>
+                SUBMIT
+              </button>
+            ) : (
+              <LoadingElement />
+            )}
+          </main>
+        </article>
+      </section>
     );
   }
 }
